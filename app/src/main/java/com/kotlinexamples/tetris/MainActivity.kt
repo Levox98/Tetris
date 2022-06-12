f9b71b8629919263d513835101a61861454ee694
Levox98 <Levox98@users.noreply.github.com>
2022-06-12 10:36:30 +0500

    Initial commit

---
+++ app/src/main/java/com/kotlinexamples/tetris/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.kotlinexamples.tetris
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import com.kotlinexamples.tetris.storage.AppPreferences
+
+class MainActivity : AppCompatActivity() {
+    var tvHighScore: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        val btnStartGame = findViewById<Button>(R.id.btn_new_game)
+        val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
+        val btnExit = findViewById<Button>(R.id.btn_exit)
+
+        btnStartGame.setOnClickListener(this::onBtnNewGameClick)
+        btnResetScore.setOnClickListener(this::onBtnResetScoreClick)
+        btnExit.setOnClickListener(this::onBtnExitClick)
+    }
+
+    private fun onBtnNewGameClick(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun onBtnResetScoreClick(view: View) {
+        val preferences = AppPreferences(this)
+        preferences.clearHighScore()
+        Snackbar.make(view, "Score successfully reset", Snackbar.LENGTH_SHORT).show()
+        tvHighScore?.text = "High score: ${ preferences.getHighScore() }"
+    }
+
+    private fun onBtnExitClick(view: View) {
+        System.exit(0)
+    }
+}
---
+++ app/src/main/java/com/kotlinexamples/tetris/constants/FieldConstants.kt
@@ -0,0 +1,5 @@
+package com.kotlinexamples.tetris.constants
+
+enum class FieldConstants(val value: Int) {
+    COLUMN_COUNT(10), ROW_COUNT(20)
+}
